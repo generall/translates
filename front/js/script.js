d66ee82f2c78c6d1e2bf15e0ac0374a307e362d9
Andrey Vasnetsov <vasnetsov93@gmail.com>
2018-05-19 21:45:30 +0300

    Add missed files

---
+++ front/js/script.js
@@ -0,0 +1,55 @@
+var vueDiff = new Vue({
+    el: '#diff',
+    data: {
+        orig: "Hello, I am Andrew",
+        translation: 'Привет, я Андрей',
+        yandex_translation: '',
+        userText: "",
+        diff: [],
+        count: 0
+    },
+    methods: {
+        calcDiff: function (oldStr, newStr) {
+            var diff = JsDiff.diffWords(oldStr, newStr, { ignoreCase: true })
+            diff.forEach(element => element.hint = "...");
+            this.diff = diff;
+        },
+        updDiff: function() {
+            this.calcDiff(this.orig, this.userText)
+        },
+        loadNext: function(){
+            this.$http.get('/get_next').then(response => {
+                this.orig = response.body.en_text;
+                this.translation = response.body.ru_text_orig;
+                this.yandex_translation = response.body.ru_text_translate;
+                this.userText = '';
+                this.diff = [];
+            })
+
+        },
+        commit: function(){
+            this.$http.post('/commit', {
+                en_text: this.orig,
+                user_text: this.userText
+            }).then(response => {
+                this.orig = response.body.en_text;
+                this.translation = response.body.ru_text_orig;
+                this.yandex_translation = response.body.ru_text_translate;
+                this.userText = '';
+                this.diff = [];
+                this.count += 1;
+            })
+        },
+        showHint: function(item) {
+            console.log(item);
+            item.hint = item.value;
+        }
+    }
+})
+
+vueDiff.loadNext()
+
+// let oldStr = "Hello, I am Andrew"
+// let newStr = "Hello, I an andrew"
+
+// vueDiff.calcDiff(oldStr, newStr)
